86f56f2b3cb5ba9e35dc9c9ae3ff84a938942385
ASUS <ASUS@DESKTOP-5LUIJ39>
2022-09-20 21:13:56 +0300

    Initial commit

---
+++ CharCounter/src/main/java/com/foxminded/javaspring/CharCounter.java
@@ -0,0 +1,10 @@
+package com.foxminded.javaspring;
+
+public class CharCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CharCounter/src/test/java/com/foxminded/javaspring/CharCounterTest.java
@@ -0,0 +1,14 @@
+package com.foxminded.javaspring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CharCounterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
