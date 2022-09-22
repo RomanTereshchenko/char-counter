87a846631a171336962470dda6e23c47f8c200b9
ASUS <ASUS@DESKTOP-5LUIJ39>
2022-09-22 19:53:30 +0300

    All comments studied, agreed and implemented.

--- CharCounter/src/main/java/com/foxminded/javaspring/CharCounter.java
+++ CharCounter/src/main/java/com/foxminded/javaspring/CharCounter.java
@@ -8,58 +8,58 @@ public class CharCounter {
 		Map<String, String> cacheMap = new HashMap<>();
 		List<String> outputCountersList = new ArrayList<>();
 
+
 		for (String inputString : inputStringList) {
+			String outputString = charCounterMapToString(buildCharCounterMap(inputString), inputString);
 			if (cacheMap.containsKey(inputString)) {
 				outputCountersList.add(
-						new StringBuilder(inputString + "\n" + cacheMap.get(inputString) + "Output taken from cache\n")
+						new StringBuilder(inputString + "\n" + cacheMap.get(inputString) + " Output taken from cache%br")
 								.toString());
 			} else {
 				outputCountersList
-						.add(new StringBuilder(inputString + "\n" + buildOutputString(inputString) + "\n").toString());
+						.add(new StringBuilder(inputString + "\n" + outputString + "\n").toString());
 
-				cacheMap.put(inputString, buildOutputString(inputString));
+				cacheMap.put(inputString, outputString);
 			}
 		}
 
-		StringBuilder output = new StringBuilder();
+		return String.join("", outputCountersList);
 
-		for (String string : outputCountersList) {
-			output.append(string).toString();
-		}
 
-		return output.toString();
-
-	}
-
-	private static String buildOutputString(String inputString) {
-
-		return charCounterMapToString(buildCharCounterMap(inputString), inputString);
 	}
 
 	private static Map<Character, Integer> buildCharCounterMap(String inputString) {
 
-		List<Character> inputStringList = new ArrayList<>();
-		for (Character character : inputString.toCharArray()) {
-			inputStringList.add(character);
-		}
-
-		Set<Character> inputStringSet = new HashSet<>();
-		for (Character character : inputStringList) {
-			inputStringSet.add(character);
-		}
+//		List<Character> chars = new ArrayList<>();
+//		for (Character character : inputString.toCharArray()) {
+//			chars.add(character);
+//		}
+//
+//		Set<Character> uniqueChars = new HashSet<>(chars);
 
 		Map<Character, Integer> charCounterMap = new HashMap<>();
-
-		for (Character charInTheSet : inputStringSet) {
-			Integer charCounter = 0;
-			for (Character charInTheList : inputStringList) {
-				if (charInTheSet.equals(charInTheList)) {
-					charCounter++;
+		
+		for (Character character : inputString.toCharArray()) {
+			charCounterMap.compute(character, (key, value) -> {
+				if (Objects.isNull(value)) {
+					return 1;
+				} else {
+					return ++ value;
 				}
-				charCounterMap.put(charInTheSet, charCounter);
-
-			}
+			});
+			
 		}
+
+//		for (Character charInTheSet : uniqueChars) {
+//			Integer charCounter = 0;
+//			for (Character charInTheList : chars) {
+//				if (charInTheSet.equals(charInTheList)) {
+//					charCounter++;
+//				}
+//				charCounterMap.put(charInTheSet, charCounter);
+//
+//			}
+//		}
 		return charCounterMap;
 	}
 
@@ -85,6 +85,7 @@ public class CharCounter {
 	public static void main(String[] args) {
 
 		List<String> testList = new ArrayList<>();
+		testList.add("Hello, world!");
 		testList.add("Hello, world!");
 		testList.add("Any other String");
 
